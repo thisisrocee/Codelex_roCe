d670eb71e6109b7c5c23d59062a76cc81fc22738
ThisIsRocee <roberts.juniors@gmail.com>
2023-02-17 01:25:16 +0200

    Exercise 4 done

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ievadi savu vārdu");
+            String name = Console.ReadLine();
+            Console.WriteLine("Ievadi savu dzimšanas gadu!");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"My name is {name} and I was born in {year}.");
+        }
+    }
+}
