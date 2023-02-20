7ba68eefdcef434dbf9f44396de8c1023005c5f4
ThisIsRocee <roberts.juniors@gmail.com>
2023-02-20 15:48:19 +0200

    Exercise 3 done

--- csharp-basics/exercises/Arithmetic/SumAverageRunningInt/Program.cs
+++ csharp-basics/exercises/Arithmetic/SumAverageRunningInt/Program.cs
@@ -2,19 +2,25 @@
 
 namespace SumAverageRunningInt
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var sum = 0;
-            int average;
             const int lowerBound = 1;
             const int upperBound = 100;
 
-            for (var number = lowerBound; number <= upperBound; ++number) 
-            {
-                sum += number;
-            }
+            Console.WriteLine(SumAverageRunningInt(lowerBound, upperBound));
+        }
+
+        private static string SumAverageRunningInt(int lowerBound, int upperBound)
+        {
+            var sum = 0;
+            for (var number = lowerBound; number <= upperBound; number++) sum += number;
+            var average = (double)sum / upperBound;
+            var str = sum.ToString();
+            var result = $"The sum of {lowerBound} to {upperBound} is {str}";
+            var result1 = $"\nThe average is {average}";
+            return result + result1;
         }
     }
 }
