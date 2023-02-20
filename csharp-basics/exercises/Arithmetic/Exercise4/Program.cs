e3cb88db584ff4357a31705bf1e515df6260e248
ThisIsRocee <roberts.juniors@gmail.com>
2023-02-20 16:03:16 +0200

    Exercise 4 done

---
+++ csharp-basics/exercises/Arithmetic/Exercise4/Program.cs
@@ -0,0 +1,12 @@
+﻿var lowerBound = 1;
+var upperBound = 10;
+
+Console.WriteLine(Product1ToN(lowerBound, upperBound));
+Console.ReadKey();
+
+static int Product1ToN(int lower, int upper)
+{
+    var sum = lower;
+    for (var number = lower; number <= upper; number++) sum *= number;
+    return sum;
+}
