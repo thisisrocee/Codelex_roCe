7603d31b01b192544c23d445cf64e8edb3081fd9
ThisIsRocee <roberts.juniors@gmail.com>
2023-02-20 19:06:08 +0200

    Exercise 6 done

---
+++ csharp-basics/exercises/Arithmetic/Exercise6/Program.cs
@@ -0,0 +1,43 @@
+﻿var min = 1;
+var max = 110;
+var columns = 11;
+
+for (var i = min; i <= max; i += columns)
+{
+    for (var j = i; j < i + columns; j++)
+    {
+        var isCoza = j % 3 == 0;
+        var isLoza = j % 5 == 0;
+        var isWoza = j % 7 == 0;
+
+        switch ((isCoza ? 1 : 0) | (isLoza ? 2 : 0) | (isWoza ? 4 : 0))
+        {
+            case 1:
+                Console.Write("Coza ");
+                break;
+            case 2:
+                Console.Write("Loza ");
+                break;
+            case 3:
+                Console.Write("CozaLoza ");
+                break;
+            case 4:
+                Console.Write("Woza ");
+                break;
+            case 5:
+                Console.Write("CozaWoza ");
+                break;
+            case 6:
+                Console.Write("LozaWoza ");
+                break;
+            case 7:
+                Console.Write("CozaLozaWoza ");
+                break;
+            default:
+                Console.Write(j + " ");
+                break;
+        }
+    }
+    Console.WriteLine();
+}
+Console.ReadKey();
