96664aa036b3f112d3c13e38037570ab38d1d3d5
ThisIsRocee <roberts.juniors@gmail.com>
2023-02-21 22:03:18 +0200

    Exercise 5 done

--- csharp-basics/exercises/Arrays/Arrays/Exercise5/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise5/Program.cs
@@ -16,19 +16,15 @@ namespace Exercise5
             var indexOfElement36 = -1;
             var indexOfElement29 = -1;
 
-            /*
-            fixme
-            for (?) {
-                
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] == 36) indexOfElement36 = i;
+                if (myArray[i] == 29) indexOfElement29 = i;
             }
-            */
+
             Console.WriteLine("Index position of 36 is: " + indexOfElement36);
             Console.WriteLine("Index position of 29 is: " + indexOfElement29);
-            
-            //Expected output:
-            //Index position of 36 is: 4
-            //Index position of 29 is: 8
-            
+            Console.ReadKey();
         }
     }
 }
