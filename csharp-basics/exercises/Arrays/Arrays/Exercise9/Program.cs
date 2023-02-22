f9f85b78ba901f543e584061c909805cd23c5ee2
ThisIsRocee <roberts.juniors@gmail.com>
2023-02-22 14:49:18 +0200

    Exercise 9 done

---
+++ csharp-basics/exercises/Arrays/Arrays/Exercise9/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] arr1 = { "mavis", "senaida", "letty" };
+string[] arr2 = { "samuel", "MABELLE", "letitia", "meridith" };
+string[] arr3 = { "Slyvia", "Kristal", "Sharilyn", "Calista" };
+
+
+
+Console.WriteLine(CapMe(arr1));
+Console.WriteLine(CapMe(arr2));
+Console.WriteLine(CapMe(arr3));
+
+static string CapMe(string[] arr)
+{
+    var strArr = new string[arr.Length];
+    for (var i = 0; i< arr.Length; i++)
+    {
+        var newWord = arr[i].ToLower();
+        var firstChar = newWord[0];
+        var result = firstChar.ToString().ToUpper();
+
+        strArr[i] = result + newWord.Substring(1);
+    }
+    return string.Join(", ", strArr);
+}
