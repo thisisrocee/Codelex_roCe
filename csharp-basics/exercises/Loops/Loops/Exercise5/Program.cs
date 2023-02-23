be09ee11c6c733845b5dd7f80fe0ec2a14d4798e
thisisrocee <roberts.juniors@gmail.com>
2023-02-23 17:56:35 +0200

    Exercise 5 done

---
+++ csharp-basics/exercises/Loops/Loops/Exercise5/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter first word:");
+var firstWord = Console.ReadLine();
+Console.WriteLine("Enter second word: ");
+var secondWord = Console.ReadLine();
+
+var firstWordLen = firstWord.Length;
+var secondWordLen = secondWord.Length;
+var dotLen = 30 - (firstWordLen + secondWordLen);
+
+Console.WriteLine(String.Empty);
+Console.Write(firstWord);
+
+for (int i = 0; i < dotLen; i++)
+{
+    Console.Write(".");
+}
+
+Console.Write(secondWord);
