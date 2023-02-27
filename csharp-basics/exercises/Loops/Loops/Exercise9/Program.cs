679dfdc1fc5c75cea896e6d85660aa113a2c7e1f
ThisIsRocee <roberts.juniors@gmail.com>
2023-02-27 12:59:42 +0200

    Exercise 9 done

---
+++ csharp-basics/exercises/Loops/Loops/Exercise9/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Desired sum:");
+var userInput = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(RollTwoDice(userInput));
+
+static string RollTwoDice(int userInput)
+{
+    var rand = new Random();
+    string str;
+
+    while (true)
+    {
+        if (userInput <= 1 || userInput > 12)
+        {
+            str = "Invalid input!";
+            break;
+        }
+
+        var firstInt = rand.Next(1, 7);
+        var secondInt = rand.Next(1, 7);
+        var sumUp = firstInt + secondInt;
+
+        if (sumUp == userInput)
+        {
+            str = $"{firstInt} and {secondInt} = {userInput}";
+            break;
+        }
+
+        Console.WriteLine($"{firstInt} and {secondInt} = {sumUp}");
+    }
+
+    return str;
+}
