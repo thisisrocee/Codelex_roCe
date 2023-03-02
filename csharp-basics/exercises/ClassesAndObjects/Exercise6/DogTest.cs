cffe60898576b1f19401afd383bcd17a420eda72
ThisIsRocee <roberts.juniors@gmail.com>
2023-03-02 23:13:56 +0200

    Exercise 6 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise6/DogTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    public class DogTest
+    {
+        static void Main(string[] args)
+        {
+            Dog max = new Dog("Max", "male");
+            Dog rocky = new Dog("Rocky", "male");
+            Dog sparky = new Dog("Sparky", "male");
+            Dog buster = new Dog("Buster", "male");
+            Dog sam = new Dog("Sam", "male");
+            Dog lady = new Dog("Lady", "female");
+            Dog molly = new Dog("Molly", "female");
+            Dog coco = new Dog("Coco", "female");
+
+            max.setFather("Rocky");
+            max.setMother("Lady");
+            rocky.setFather("Sam");
+            rocky.setMother("Molly");
+            buster.setFather("Sparky");
+            buster.setMother("Lady");
+            coco.setFather("Buster");
+            coco.setMother("Molly");
+
+            Console.WriteLine(coco.FathersName());
+            Console.WriteLine(sparky.FathersName());
+            Console.WriteLine(coco.HasSameMotherAs(rocky));
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Exercise6
+{
+    class Dog
+    {
+        private string _name;
+        private string _sex;
+        private string _mother;
+        private string _father;
+
+        public Dog(string name, string sex)
+        {
+            _name = name;
+            _sex = sex;
+        }
+
+        public void setMother(string mother)
+        {
+            _mother = mother;
+        }
+
+        public void setFather(string father)
+        {
+            _father = father;
+        }
+
+        public string FathersName()
+        {
+            if (_father == null)
+                return "Unknown";
+            return _father;
+        }
+
+        public bool HasSameMotherAs(Dog otherDog)
+        {
+            if(otherDog._mother == _mother) 
+                return true;
+            return false;
+        }
+    }
+}
