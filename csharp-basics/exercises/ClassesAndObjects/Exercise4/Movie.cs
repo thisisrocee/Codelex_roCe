0c2d0038fff0c4e4c4c31b54f600683fcbcd654e
ThisIsRocee <roberts.juniors@gmail.com>
2023-03-02 21:06:33 +0200

    Exercise 4 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Movie.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+
+namespace Exercise4
+{
+    class Movie
+    {
+        public string _title;
+        private string _studio;
+        private string _rating;
+
+        public Movie(string title, string studio, string rating)
+        {
+            _title = title;
+            _studio = studio;
+            _rating = rating;
+        }
+
+        public Movie(string title, string studio)
+        {
+            _title = title;
+            _studio = studio;
+            _rating = "PG";
+        }
+
+        public static Movie[] GetPG(Movie[] movies)
+        {
+            List<Movie> newMoviesArr = new List<Movie>();
+
+            foreach (Movie movie in movies)
+            {
+                if (movie._rating == "PG")
+                {
+                    newMoviesArr.Add(movie);
+                }
+            }
+
+            return newMoviesArr.ToArray();
+        }
+}
+}
+
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/TestMovie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise4
+{
+    internal class TestMovie
+    {
+        static void Main(string[] args)
+        {
+            Movie casionRoyale = new Movie("Casino Royale", "Eon Productions", "PG13");
+            Movie glass = new Movie("Glass", "Buena Vista International", "PG13");
+            Movie spiderMan = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG");
+
+            Movie[] movies = { casionRoyale, glass, spiderMan };
+            Movie[] pgMovie = Movie.GetPG(movies);
+
+            Console.WriteLine("Movies with PG rating:");
+
+            foreach (Movie movie in pgMovie)
+            {
+                Console.WriteLine(movie._title);
+            }
+        }
+    }
+}
