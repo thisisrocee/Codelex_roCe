7063aea563d6c6f66958d30606857d116f1c9224
ThisIsRocee <roberts.juniors@gmail.com>
2023-03-03 01:05:45 +0200

    Exercise 9 done

---
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/Bank.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class Bank
+    {
+        private string _name;
+        private double _balance;
+
+        public Bank(string name, double balance)
+        {
+            _name = name;
+            _balance = balance;
+        }
+
+        public string ShowUserNameAndBalance()
+        {
+            return $"{_name}, {_balance.ToString("$#,##0.00;-$#,##0.00")}";
+        }
+    }
+}
