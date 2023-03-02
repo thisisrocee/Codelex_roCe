489bcf4f7b02436e693186615c1c7f07be16680d
ThisIsRocee <roberts.juniors@gmail.com>
2023-03-03 00:46:51 +0200

    Exercise 8 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise8
+{
+    class Point
+    {
+        public int _x;
+        public int _y;
+        public Point(int x, int y)
+        {
+            _x = x; 
+            _y = y;
+        }
+
+        public void SwapPoints(Point other)
+        {
+            var x1 = _x;
+            var y1 = _y;
+
+            _x = other._x;
+            _y = other._y;
+
+            other._x  = x1;
+            other._y = y1;
+        }
+    }
+}
