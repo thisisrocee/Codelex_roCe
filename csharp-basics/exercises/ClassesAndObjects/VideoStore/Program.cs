c0acd2419e93483b123934b0bd23812562546f03
ThisIsRocee <roberts.juniors@gmail.com>
2023-03-03 15:18:50 +0200

    Exercise 10 done

--- csharp-basics/exercises/ClassesAndObjects/VideoStore/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/VideoStore/Program.cs
@@ -12,8 +12,35 @@ namespace VideoStore
         private static VideoStore _videoStore = new VideoStore();
         private static void Main(string[] args)
         {
+            _videoStore.AddVideo("The Matrix");
+            _videoStore.AddVideo("Godfather II");
+            _videoStore.AddVideo("Star Wars Episode IV: A New Hope");
+
+            _videoStore.TakeUsersRating(2, "The Matrix");
+            _videoStore.TakeUsersRating(3, "The Matrix");
+            _videoStore.TakeUsersRating(4, "Godfather II");
+            _videoStore.TakeUsersRating(5, "Godfather II");
+            _videoStore.TakeUsersRating(6, "Star Wars Episode IV: A New Hope");
+            _videoStore.TakeUsersRating(7, "Star Wars Episode IV: A New Hope");
+
+            _videoStore.Checkout("The Matrix");
+            _videoStore.ReturnVideo("The Matrix");
+
+            _videoStore.Checkout("Godfather II");
+            _videoStore.ReturnVideo("Godfather II");
+
+            _videoStore.Checkout("Star Wars Episode IV: A New Hope");
+            _videoStore.ReturnVideo("Star Wars Episode IV: A New Hope");
+
+            _videoStore.Checkout("Godfather II");
+            Console.WriteLine("Inventory after renting Godfather II:");
+            _videoStore.ListInventory();
+
+            Console.ReadLine();
+
             while (true)
             {
+                Console.WriteLine("=========================================");
                 Console.WriteLine("Choose the operation you want to perform ");
                 Console.WriteLine("Choose 0 for EXIT");
                 Console.WriteLine("Choose 1 to fill video store");
--- csharp-basics/exercises/ClassesAndObjects/VideoStore/VideoStore.cs
+++ csharp-basics/exercises/ClassesAndObjects/VideoStore/VideoStore.cs
@@ -5,34 +5,73 @@ namespace VideoStore
 {
     class VideoStore
     {
-
-        public VideoStore()
-        {
-            
-        }
+        private List<Video> _videos = new List<Video>();
 
         public void AddVideo(string title)
         {
-            
+            _videos.Add(new Video(title));
         }
-        
+
         public void Checkout(string title)
         {
+            var video = _videos.Find(v => v.Title == title);
 
+            if (video == null)
+            {
+                Console.WriteLine("Video not found.");
+                return;
+            }
+
+            if (!video.Availability)
+            {
+                Console.WriteLine("Video is already checked out.");
+                return;
+            }
+
+            video.Availability = false;
+            Console.WriteLine("Video checked out successfully.");
         }
 
         public void ReturnVideo(string title)
         {
+            Video video = _videos.Find(v => v.Title == title);
+
+            if (video == null)
+            {
+                Console.WriteLine("Video not found.");
+                return;
+            }
+
+            if (video.Availability)
+            {
+                Console.WriteLine("Video is not checked out.");
+                return;
+            }
+
+            video.Availability = true;
+            Console.WriteLine("Video returned successfully.");
         }
 
         public void TakeUsersRating(double rating, string title)
         {
-            
+            Video video = _videos.Find(v => v.Title == title);
+
+            if (video == null)
+            {
+                Console.WriteLine("Video not found.");
+                return;
+            }
+
+            video.ReceivingRating(rating);
+            Console.WriteLine($"Rating of {rating} added to {title}");
         }
 
         public void ListInventory()
         {
-            
+            foreach (var video in _videos)
+            {
+                Console.WriteLine(video);
+            }
         }
     }
 }
