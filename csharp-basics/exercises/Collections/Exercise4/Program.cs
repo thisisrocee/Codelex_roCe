e2156de80ede489dab99c6c36e6ca2910fed8393
ThisIsRocee <roberts.juniors@gmail.com>
2023-03-08 21:45:46 +0200

    Exercise 4 done

---
+++ csharp-basics/exercises/Collections/Exercise4/Program.cs
@@ -0,0 +1,16 @@
+﻿var namesList = new HashSet<string>();
+
+while (true)
+{
+    Console.Write("Enter name:");
+    var name = Console.ReadLine();
+
+    if (name == "")
+    {
+        break;
+    }
+
+    namesList.Add(name);
+}
+
+Console.WriteLine($"Unique name list contains: {string.Join(", ", namesList)}");
