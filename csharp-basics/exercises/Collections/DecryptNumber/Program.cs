1ca50586a14f0a28f9d0045df893e15f3fdf6f58
ThisIsRocee <roberts.juniors@gmail.com>
2023-03-08 21:22:02 +0200

    Exercise 3 done

--- csharp-basics/exercises/Collections/DecryptNumber/Program.cs
+++ csharp-basics/exercises/Collections/DecryptNumber/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DecryptNumber
 {
@@ -16,6 +17,27 @@ namespace DecryptNumber
                 "!)(^&(#@",
                 "!)(#&%(*@#%"
             };
+
+            var newStr = new List<string>();
+
+            foreach (var word in cryptedNumbers)
+            {
+                var word1 = word.Select(s => (
+                    s == '!' ? '1' :
+                    s == '@' ? '2' :
+                    s == '#' ? '3' :
+                    s == '$' ? '4' :
+                    s == '%' ? '5' :
+                    s == '^' ? '6' :
+                    s == '&' ? '7' :
+                    s == '*' ? '8' :
+                    s == '(' ? '9' :
+                    s == ')' ? '0' : s)).ToList();
+
+                newStr.Add(new string(word1.ToArray()));
+            }
+
+            Console.WriteLine(string.Join(" ", newStr));
         }
     }
 }
