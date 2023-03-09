299365babcf8ce381c48d3661deca21e8ce8c679
ThisIsRocee <roberts.juniors@gmail.com>
2023-03-09 20:36:17 +0200

    Exercise 9 done

--- csharp-basics/exercises/Collections/Phonebook/PhoneDirectory.cs
+++ csharp-basics/exercises/Collections/Phonebook/PhoneDirectory.cs
@@ -1,65 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 namespace PhoneBook
 {
     public class PhoneDirectory
     {
-        private PhoneEntry[] _data;
-        private int _dataCount;
+        private SortedDictionary<string, PhoneEntry> _data;
 
         public PhoneDirectory() {
-            _data = new PhoneEntry[1];
-            _dataCount = 0;
-        }
-
-        private int Find(string name) {
-            for (var i = 0; i < _dataCount; i++) 
-            {
-                if (_data[i].name.Equals(name)) 
-                {
-                    return i;
-                }
-            }
-
-            return -1;
+            _data = new SortedDictionary<string, PhoneEntry>();
         }
 
         public string GetNumber(string name) 
         {
-            var position = Find(name);
-            if (position == -1) 
+            if (_data.TryGetValue(name, out var entry))
             {
-                return null;
-            } 
-            else 
-            {
-                return _data[position].number;
+                return entry.number;
             }
+            return null;
         }
 
         public void PutNumber(string name, string number) 
         {
-            if (name == null || number == null) 
+            if (name == null || number == null)
             {
-                throw new Exception("name and number cannot be null");
+                throw new Exception("name or number cannot be null");
             }
-
-            var i = Find(name);
-            if (i >= 0) 
-            {
-                _data[i].number = number;
-            }
-            else 
-            {
-                if (_dataCount == _data.Length) 
-                {
-                    Array.Resize(ref _data, (2 * _data.Length));
-                }
-
-                var newEntry = new PhoneEntry {name = name, number = number}; // Create a new pair.
-                _data[_dataCount] = newEntry;   // Add the new pair to the array.
-                _dataCount++;
-            }
+            _data[name] = new PhoneEntry { name = name, number = number };
         }
     }
 }
