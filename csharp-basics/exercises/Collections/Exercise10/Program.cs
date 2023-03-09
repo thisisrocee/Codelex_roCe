6ef9f26fca56f42597a64c15ad44b4271f95cd99
ThisIsRocee <roberts.juniors@gmail.com>
2023-03-09 20:48:47 +0200

    Exercise 10 done

---
+++ csharp-basics/exercises/Collections/Exercise10/Program.cs
@@ -0,0 +1,19 @@
+﻿var bmwHashSet = new HashSet<string>();
+
+bmwHashSet.Add("e30");
+bmwHashSet.Add("e36");
+bmwHashSet.Add("e46");
+bmwHashSet.Add("e90");
+bmwHashSet.Add("f30");
+
+Console.WriteLine($"HashSet: {string.Join(", ", bmwHashSet)}");
+
+bmwHashSet.Clear();
+
+Console.WriteLine($"All items removed HashSet: {string.Join(", ", bmwHashSet)}");
+
+bmwHashSet.Add("e30");
+bmwHashSet.Add("e30");
+
+Console.WriteLine($"HashSet: {string.Join(", ", bmwHashSet)} \n" +
+                  $"no, it is not possible to add duplicates, because it is HashSet :)");
