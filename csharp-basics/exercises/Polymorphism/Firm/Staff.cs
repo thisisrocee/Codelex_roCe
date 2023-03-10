0c02bfa3d7997d6db141452a86e374e8e42f6251
ThisIsRocee <roberts.juniors@gmail.com>
2023-03-10 23:37:59 +0200

    Exercise 4 done

--- csharp-basics/exercises/Polymorphism/Firm/Staff.cs
+++ csharp-basics/exercises/Polymorphism/Firm/Staff.cs
@@ -6,12 +6,9 @@ namespace Firm
     {
         StaffMember[] staffList;
 
-        //-----------------------------------------------------------------
-        // Sets up the list of staff members.
-        //-----------------------------------------------------------------
         public Staff()
         {
-            staffList = new StaffMember[6];
+            staffList = new StaffMember[8];
             staffList[0] = new Executive("Sam", "123 Main Line",
                 "555-0469", "123-45-6789", 2423.07);
             staffList[1] = new Employee("Carla", "456 Off Line",
@@ -24,13 +21,19 @@ namespace Firm
                 "555-8374");
             staffList[5] = new Volunteer("Cliff", "321 Duds Lane",
                 "555-7282");
+            staffList[6] = new Commission("Steve", "420 bruh Lane",
+                "555-2345", "535-34-7534", 6.25, 0.2);
+            staffList[7] = new Commission("Burton", "234 Duds Lane",
+                "555-2556", "535-34-7534", 9.75, 0.15);
+
             ((Executive) staffList[0]).AwardBonus(500.00);
             ((Hourly) staffList[3]).AddHours(40);
+            ((Commission) staffList[6]).AddHours(35);
+            ((Commission) staffList[6]).AddSales(400);
+            ((Commission) staffList[7]).AddHours(40);
+            ((Commission) staffList[7]).AddSales(950);
         }
 
-        //-----------------------------------------------------------------
-        // Pays all staff members.
-        //-----------------------------------------------------------------
         public void Payday()
         {
             foreach (var staff in staffList)
@@ -40,7 +43,7 @@ namespace Firm
                 if (amount == 0.00)
                     Console.WriteLine("Thanks!");
                 else
-                    Console.WriteLine("Paid: " + amount);
+                    Console.WriteLine("Paid: $" + amount);
                 Console.WriteLine("-----------------------------------");
             }
         }
